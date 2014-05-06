cab90caa020428e5ca7412d226906545a7f2f98a
Anil Dash <anil@dashes.com>
2014-05-06 16:51:58 -0400

    slight copy changes

--- config.inc.php
+++ config.inc.php
@@ -8,12 +8,12 @@
 
 if (date('Y-m-d') == '2014-05-07') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New! ThinkUp insights now celebrate your <a href="http://blog.thinkup.com/post/84828127412/the-f-you-say" style="color: #2785D3;">LOLs, thanks, and F-bombs</a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New! ThinkUp insights now notice your <a href="http://blog.thinkup.com/post/84828127412/the-f-you-say" style="color: #2785D3;">LOLs, thanks, and F-bombs</a>.';
 }
 
 if (date('Y-m-d') == '2014-05-08') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on our blog: <a href="http://blog.thinkup.com/post/84831591137/thinkups-investor-update-for-april-2014" style="color: #2785D3;">ThinkUp’s investor update for April 2014</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'We just shared <a href="http://blog.thinkup.com/post/84831591137/thinkups-investor-update-for-april-2014" style="color: #2785D3;">ThinkUp’s investor update for April 2014</a>';
 }
 
 if (date('Y-m-d') == '2014-05-09') {
