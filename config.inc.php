323f6f0301daafabfc9a0a5694904cc2819f2774
Anil Dash <anil@dashes.com>
2014-11-30 17:00:02 -0500

    Touts for EOY insights

--- config.inc.php
+++ config.inc.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * To see what your tout copy will look like in an email, set if date line to if (true), then run
- * 
+ *
  * $ TEST_DEBUG=1 php webapp/plugins/insightsgenerator/tests/TestOfInsightsGeneratorPlugin.php -t  testMandrillHTMLThinkUpLLCUnsubLinkWithOutWelcomeMessage > webapp/email.html
- * 
+ *
  */
 
 if (date('Y-m-d') == '2014-05-07') {
@@ -19,5 +19,72 @@ if (date('Y-m-d') == '2014-05-08') {
 if (date('Y-m-d') == '2014-05-09') {
 //if (true) {
     $THINKUP_CFG['thinkupllc_email_tout'] = 'Well, look at that: <a href="http://blog.thinkup.com/post/84453987487/sharing-insights-just-got-easier" style="color: #2785D3;">Sharing ThinkUp insights just got easier.</a>';
+}
+
+if (date('Y-m-d') == '2014-12-01') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Coming up tomorrow in <a href="{$site_root_path}2014" style="color: #2785D3;">your Best of 2014</a>: All about you!';
+}
+
+if (date('Y-m-d') == '2014-12-02') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Tomorrow\'s <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a>: Your chattiest day.';
+}
+
+if (date('Y-m-d') == '2014-12-03') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Look out tomorrow for your Photo of the Year in <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a>.';
+}
+
+if (date('Y-m-d') == '2014-12-04') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'What was really exciting in 2014? Check out tomorrow\'s <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a>!';
+}
+
+if (date('Y-m-d') == '2014-12-05') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Coming up on Monday, even more <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a>.';
+}
+
+if (date('Y-m-d') == '2014-12-08') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Who was your BFF for 2014? Find out in tomorrow\'s <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a>!';
+}
+
+if (date('Y-m-d') == '2014-12-09') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Tomorrow\'s <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a> covers every last word of 2014.';
+}
+
+if (date('Y-m-d') == '2014-12-10') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Up next in <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a>: What <em>really</em> got a response?';
+}
+
+if (date('Y-m-d') == '2014-12-11') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Who got link love in 2014? Find out in <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a> tomorrow!';
+}
+
+if (date('Y-m-d') == '2014-12-12') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Stay tuned to <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a> &mdash; this next one will make you smile.';
+}
+
+if (date('Y-m-d') == '2014-12-14') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Tomorrow in <a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a>: Your link of the year.';
+}
+
+
+
+if (date('Y-m-d') == '2014-12-22') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = '<a href="{$site_root_path}2014" style="color: #2785D3;">Best of 2014</a> ends tomorrow with your best of the year!';
+}
+
+if (date('Y-m-d') == '2014-12-31') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Thanks for a wonderful 2014! We\'ll see you next year.';
 }
 
