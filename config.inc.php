9796435929ae07a2d39dbeb279bcad70b0778940
Anil Dash <anil@dashes.com>
2015-06-30 23:46:44 -0400

    Tout for launching out of beta
    * Instagram support
    * Follower search
    * Link to post

--- config.inc.php
+++ config.inc.php
@@ -6,58 +6,9 @@
  *
  */
 
-if (date('Y-m-d') == '2014-12-03') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Tomorrow in Best of 2014: Look out for your Photo of the Year.';
-}
 
-if (date('Y-m-d') == '2014-12-04') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'What was really exciting in 2014? Check out tomorrow\'s Best of 2014!';
-}
-
-if (date('Y-m-d') == '2014-12-05') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Coming up on Monday: Even more Best of 2014.';
-}
-
-if (date('Y-m-d') == '2014-12-08') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Who was your BFF this year? Find out in tomorrow\'s Best of 2014!';
-}
-
-if (date('Y-m-d') == '2014-12-09') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Tomorrow\'s Best of 2014 covers every last word of your year.';
-}
-
-if (date('Y-m-d') == '2014-12-10') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Up next in Best of 2014: What <em>really</em> got a response?';
-}
-
-if (date('Y-m-d') == '2014-12-11') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Who got link love this year? Find out in Best of 2014 tomorrow!';
-}
-
-if (date('Y-m-d') == '2014-12-12') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Stay tuned to Best of 2014 &mdash; this next one will make you smile.';
-}
-
-if (date('Y-m-d') == '2014-12-14') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Tomorrow in Best of 2014: Your link of the year.';
-}
-
-if (date('Y-m-d') == '2014-12-22') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Best of 2014 ends tomorrow with your best of the year!';
-}
-
-if (date('Y-m-d') == '2014-12-31') {
-//if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'Thanks for a wonderful 2014! We\'ll see you next year.';
+if (date('Y-m-d') == '2015-07-01') {
+// if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW: Search your followers and get insights for Instagram! <a href="http://blog.thinkup.com/post/122853360732/super-powers-for-your-social-networks-thinkups"><strong style="color: black;">Check it out.</strong></a>';
 }
 
