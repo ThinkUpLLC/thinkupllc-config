ff6af566b78f9dd994f0eb098855e572b5165671
Anil Dash <anil@dashes.com>
2015-07-02 11:51:19 -0400

    Week of instagram + search promos

--- config.inc.php
+++ config.inc.php
@@ -7,8 +7,28 @@
  */
 
 
-if (date('Y-m-d') == '2015-07-01') {
+if (date('Y-m-d') == '2015-07-06') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW: Search your followers and get insights for Instagram! <a href="http://blog.thinkup.com/post/122853360732/super-powers-for-your-social-networks-thinkups"><strong style="color: black;">Check it out.</strong></a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW: Search your followers! <a href="http://blog.thinkup.com/post/122850636847/seven-powerful-ways-to-search-your-followers"><strong style="color: black;">Learn how to do it.</strong></a>';
+}
+
+if (date('Y-m-d') == '2015-07-07') {
+// if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Get insights about your Instagram account! <a href="http://blog.thinkup.com/post/122850640332/thinkup-makes-instagram-even-happier"><strong style="color: black;">Find out all about it.</strong></a>';
+}
+
+if (date('Y-m-d') == '2015-07-08') {
+// if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Find out so much more about your followers <a href="http://blog.thinkup.com/post/122850636847/seven-powerful-ways-to-search-your-followers"><strong style="color: black;">using new ThinkUp search.</strong></a>';
+}
+
+if (date('Y-m-d') == '2015-07-09') {
+// if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Use Instagram? <a href="http://blog.thinkup.com/post/122850640332/thinkup-makes-instagram-even-happier"><strong style="color: black;">ThinkUp now has Instagram support!</strong></a>';
+}
+
+if (date('Y-m-d') == '2015-07-10') {
+// if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'Search your followers and get insights for Instagram! <a href="http://blog.thinkup.com/post/122853360732/super-powers-for-your-social-networks-thinkups"><strong style="color: black;">A FREE upgrade for you.</strong></a>';
 }
 
