cfe54b128e609608568725c5fed580abd64eb981
Gina Trapani <ginatrapani@gmail.com>
2015-10-28 10:21:04 -0400

    Add tracking tags to email touts

--- config.inc.php
+++ config.inc.php
@@ -9,147 +9,147 @@
 
 if (date('Y-m-d') == '2015-10-28') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?tu=1028a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?tu=1028b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-10-29') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?tu=1029a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-10-30') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?tu=1030a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?tu=1030b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-10-31') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?tu=1031a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?tu=1031b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-01') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?tu=1101a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?tu=1101b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-02') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?tu=1102a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?tu=1102b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-03') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?tu=1103a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-11-04') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?tu=1104a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?tu=1104b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-05') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?tu=1105a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?tu=1105b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-06') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?tu=1106a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?tu=1106b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-07') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?tu=1107a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?tu=1107b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-08') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?tu=1108a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-11-09') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?tu=1109a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?tu=1109b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-10') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?tu=1110a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?tu=1110b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-11') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?tu=1111a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?tu=1111b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-12') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?tu=1112a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?tu=1112b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-13') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?tu=1113a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-11-14') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?tu=1114a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?tu=1114b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-15') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?tu=1115a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?tu=1115b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-16') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?tu=1116a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?tu=1116b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-17') {
 // if (true) {7
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?tu=1117a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?tu=1117b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-18') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?tu=1118a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-11-19') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?tu=1119a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?tu=1119b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-20') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?tu=1120a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?tu=1120b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-21') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?tu=1121a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?tu=1121b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-22') {
 // if (true) {7
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?tu=1122a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?tu=1122b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-23') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?tu=1123a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-11-24') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?tu=1124a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?tu=1124b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-11-25') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?tu=1125a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?tu=1125b" style="color: black;">Add your projects now.</a>';
 }
 
 // Thanksgiving Day
