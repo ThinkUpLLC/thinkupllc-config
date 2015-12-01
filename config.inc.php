f3471a66a821a4f5fa7686604054c7d0529bbef5
Gina Trapani <ginatrapani@gmail.com>
2015-12-01 17:02:09 -0500

    Fix GA tag dates on Makerbase touts

--- config.inc.php
+++ config.inc.php
@@ -7,112 +7,112 @@
  */
 
 
-//if (date('Y-m-d') == '2015-12-02') {
-if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1124a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their makers. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1124b" style="color: black;">Find your favorites.</a>';
+if (date('Y-m-d') == '2015-12-02') {
+//if (true) {
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1202a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their makers. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1202b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-03') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1125a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1125b" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1203a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1203b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-04') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1111a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1111b" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1204a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1204b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-05') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1112a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1112b" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1205a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1205b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-06') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1106a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1106b" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1206a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1206b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-07') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1107a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1107b" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1207a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1207b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-08') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1108a"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1208a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-12-09') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1109a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1109b" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1209a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1209b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-10') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1110a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1110b" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1210a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1210b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-11') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1111a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1111b" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1211a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1211b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-12') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1112a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1112b" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1212a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1212b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-13') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1113a"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1213a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-12-14') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1114a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1114b" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1214a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1214b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-15') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1115a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1115b" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1215a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1215b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-16') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1116a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1116b" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1216a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1216b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-17') {
 // if (true) {7
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1117a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1117b" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1217a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1217b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-18') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1118a"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1218a"><strong style="color: black;">Makerbase</strong></a>.';
 }
 
 if (date('Y-m-d') == '2015-12-19') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1119a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1119b" style="color: black;">Find your favorites.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1219a"><strong style="color: black;">Makerbase</strong></a> is a database of digital projects and their creators. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1219b" style="color: black;">Find your favorites.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-20') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1120a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1120b" style="color: black;">Add your projects now.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW! Anyone can edit <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1220a"><strong style="color: black;">Makerbase</strong></a>, a directory of people who make things out of bits &amp; bytes. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1220b" style="color: black;">Add your projects now.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-21') {
 //if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1121a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1121b" style="color: black;">how they inspire you.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'New on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1221a"><strong style="color: black;">Makerbase</strong></a>: tell your favorite digital creators <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1221b" style="color: black;">how they inspire you.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-22') {
 // if (true) {7
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1122a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1122b" style="color: black;">Check it out.</a>';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'NEW from your friends at ThinkUp: <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1222a"><strong style="color: black;">Makerbase</strong></a> is like IMDb for apps &amp; web sites. <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1222b" style="color: black;">Check it out.</a>';
 }
 
 if (date('Y-m-d') == '2015-12-23') {
 // if (true) {
-    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1123a"><strong style="color: black;">Makerbase</strong></a>.';
+    $THINKUP_CFG['thinkupllc_email_tout'] = 'You make great things. Tell the world what you made on <a href="https://makerba.se/?utm_source=ThinkUp&utm_medium=Email&utm_campaign=Tout&utm_content=1223a"><strong style="color: black;">Makerbase</strong></a>.';
 }
